f066a55da32eed3e0fed7ecd93e1695783d97736
anastasiia ihnatenko <199234@edu.p.lodz.pl>
2017-10-12 19:17:56 +0200

    fixed problem with updating visability of text boxes

--- src/gui/GraphParametersDialog.java
+++ src/gui/GraphParametersDialog.java
@@ -189,6 +189,6 @@ public class GraphParametersDialog extends Dialog {
 	private void updateVisabilityOfTextBoxes() {
 		if(selectedIndex == 0 || selectedIndex == 1 || selectedIndex == 8 || selectedIndex == 9 || selectedIndex == 10) {
 			TTextBox.setEnabled(false);
-		}
+		}else TTextBox.setEnabled(true);
 	}
 }
