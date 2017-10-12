2a47f19b64ab4bc5f17fb52f92ab0467c6248a8c
anastasiia ihnatenko <199234@edu.p.lodz.pl>
2017-10-12 23:44:41 +0200

    added new cases

--- src/common/SignalFactory.java
+++ src/common/SignalFactory.java
@@ -3,7 +3,7 @@ package common;
 public class SignalFactory {
 	
 	public static Signal getSignal(String signalType, int f, double A, int t1, 
-			double d, double T) {
+			double d, double T, double ts, double ns, double p) {
 		switch(signalType) {
 		case "(S1) szum o rozk\u0142adzie jednostajnym":
 			return new ConstNoise(A, t1, f, d);		
@@ -21,6 +21,12 @@ public class SignalFactory {
 			return new RectangleSymetrical(T, A, t1, f, d);
 		case "(S8) sygna\u0142 tr\u00F3jk\u0105tny":
 			return new Triangle(T, A, t1, f, d);
+		case "(S9) skok jednostkowy":
+			return new UnitJump(ts, A, t1, f, d);
+		case "(S10) impuls jednostkowy":
+			return new UnitImpuls(ts, A, t1, f, d);
+		case "(S11) szum impulsowy":
+			return new ImpulsNoise(p, A, t1, f, d);
 		default:
 			return null;
 		}
