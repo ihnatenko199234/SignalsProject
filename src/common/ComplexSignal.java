438bf73407a99492f76e5f079178ec72957e7ff3
anastasiia ihnatenko <199234@edu.p.lodz.pl>
2017-10-16 16:22:55 +0200

    added new class for complex signal

---
+++ src/common/ComplexSignal.java
@@ -0,0 +1,16 @@
+package common;
+
+public class ComplexSignal extends Signal{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public double[][] generateSignal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
