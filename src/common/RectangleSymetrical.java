6a0130610861c22f8965a1d1e98f243cf4543d48
anastasiia ihnatenko <199234@edu.p.lodz.pl>
2017-10-16 13:28:47 +0200

    added kw parameter

--- src/common/RectangleSymetrical.java
+++ src/common/RectangleSymetrical.java
@@ -1,9 +1,14 @@
 package common;
-public class RectangleSymetrical extends PeriodicSignal {
 
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
+public class RectangleSymetrical extends PeriodicSignal {
+	@XStreamOmitField
+	private double kw;
 	public RectangleSymetrical(double T, double A, int t1, int f, double d, double kw) {
 		super(T, A, t1, f, d);
 		name = "S7";
+		this.kw = kw;
 	}
 
 	@Override
@@ -16,7 +21,7 @@ public class RectangleSymetrical extends PeriodicSignal {
 		for(int i = 0; i < d*f; i++) {
 			tab[i][0] = t1 + (1.0/f * i); 
 			
-			if((1.0/f * i) % T >= t1 && (1.0/f * i) % T < 1.0/2 * T + t1) {
+			if((1.0/f * i) % T >= t1 && (1.0/f * i) % T < kw * T + t1) {
 			  tab[i][1] = A;// * Math.sin( (2*Math.PI / T) * ( 1.0/f * i - t1));
 			}
 			else {
