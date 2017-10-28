7662a8879c3f07adf09f08e50af32f491d7e71b5
anastasiia ihnatenko <199234@edu.p.lodz.pl>
2017-10-28 23:50:21 +0200

    added measures calculation

---
+++ src/common/Measures.java
@@ -0,0 +1,51 @@
+package common;
+
+public class Measures {
+
+	public static double MSE(double[][] array1, double[][] array2) {
+		double sum = 0;
+		
+		for(int i=0; i<array1.length; i++) {
+			sum+= Math.pow((array1[i][1] - array2[i][1]), 2);
+		}
+
+		double result = sum/array1.length;
+		
+		return result;
+	}
+	
+	public static double SNR(double[][] array1, double[][] array2) {
+		double sum1 = 0;
+		double sum2 = 0;
+		for(int i=0; i<array1.length; i++) {
+			sum1+= Math.pow(array1[i][1], 2);
+			sum2+= Math.pow((array1[i][1] - array2[i][1]), 2);
+		}
+		double result = 10*Math.log10(sum1/sum2);
+		
+		return result;
+	}
+	
+	public static double PSNR(double[][] array1, double[][] array2) {
+		double max = array1[0][1];
+		
+		for(int i=1; i<array1.length; i++) {
+			if(array1[i][1] > max) max = array1[i][1];
+		}
+		double result = 10*Math.log10(max/MSE(array1,array2));
+		
+		return result;
+	}
+	
+	public static double MD(double[][] array1, double[][] array2) {
+		double max = Math.abs(array1[0][1] - array2[0][1]);
+		double current;
+		
+		for(int i=1; i<array1.length; i++) {
+			current = Math.abs(array1[i][1] - array2[i][1]);
+			if(current > max) max = current;
+		}
+		
+		return max;
+	}
+}
